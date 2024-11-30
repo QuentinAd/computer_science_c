feddd873a96e594fda9c4624e0b5a91a68c2be5a
QuentinAd <qadam2602@gmail.com>
2024-11-30 21:28:55 +0100

    simple array creation using malloc and pointer arithmetics

---
+++ data_structures/array/array.c
@@ -0,0 +1,21 @@
+
+#include <stdio.h> // for printf
+#include <stdlib.h> // for malloc
+
+int main(void) {
+    // initialize the array
+    int *arr = malloc(3 * sizeof(int));
+    // if allocation fails
+    if (arr == NULL) {
+        return 1;
+    }
+    // initialize the array manually for the sake of example
+    *(arr + 0) = 1; // arr[0]
+    *(arr + 1) = 2; // arr[1]
+    *(arr + 2) = 3; // arr[2]
+
+    // print each value of the array
+    for (int i = 0; i < 3; i++){
+        printf("%i\n", *(arr + i));
+    }
+}
