86035e5f4b17c2ad2d7a94c7fb7012db50d09de9
Quentin Adam <35724429+QuentinAd@users.noreply.github.com>
2025-07-02 20:48:00 +0200

    Ignore build artifacts

--- problem_sets/leap/test.c
+++ problem_sets/leap/test.c
@@ -6,20 +6,20 @@ void setUp(void) {}
 void tearDown(void) {}
 
 void test_leap_year() {
-    TEST_ASSERT_TRUE(is_leap_year(2000));
-    TEST_ASSERT_FALSE(is_leap_year(1900));
-    TEST_ASSERT_TRUE(is_leap_year(2024));
-    TEST_ASSERT_FALSE(is_leap_year(2023));
+  TEST_ASSERT_TRUE(is_leap_year(2000));
+  TEST_ASSERT_FALSE(is_leap_year(1900));
+  TEST_ASSERT_TRUE(is_leap_year(2024));
+  TEST_ASSERT_FALSE(is_leap_year(2023));
 }
 
 void test_edge_cases() {
-    TEST_ASSERT_TRUE(is_leap_year(2400));
-    TEST_ASSERT_FALSE(is_leap_year(2100));
+  TEST_ASSERT_TRUE(is_leap_year(2400));
+  TEST_ASSERT_FALSE(is_leap_year(2100));
 }
 
 int main() {
-    UNITY_BEGIN();
-    RUN_TEST(test_leap_year);
-    RUN_TEST(test_edge_cases);
-    return UNITY_END();
+  UNITY_BEGIN();
+  RUN_TEST(test_leap_year);
+  RUN_TEST(test_edge_cases);
+  return UNITY_END();
 }
